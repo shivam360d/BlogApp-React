0f4d2f3b3ded60476b6e03c683cf1bd0938e1e0b
Shivam Vats <shivam88y@yahoo.com>
2019-03-23 00:50:06 +0530

    adding thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
-export const fetchPosts=()=>{
-    return{
-        type:'FETCH_POST'
-    };
+import jsonPlaceholer from '../api/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+        const response =await jsonPlaceholer.get('/posts');
+        dispatch({type:'FETCH_POSTS',payload:response})
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,6 +4,7 @@ import { fetchPosts } from '../actions';
 
 class PostList extends React.Component{
     componentDidMount=()=>{
+        //calling action creator and in the action creator api calling will be done!
         this.props.fetchPosts();
     }
     render(){
