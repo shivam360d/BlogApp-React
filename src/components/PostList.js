fde56b47834989b4700da479b965ca2a18f53d6c
Shivam Vats <shivam88y@yahoo.com>
2019-03-25 17:47:14 +0530

    rendering and getting UserHeader

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component{
     componentDidMount=()=>{
@@ -15,6 +16,7 @@ renderList= ()=>{
                 <i className="large middle aligned icon user" />
                 <div className="content">
                 <div className="description"><h2>{post.title}</h2><p>{post.body}</p></div>
+                <UserHeader userId={post.userId} />
                 </div>
             </div>         
         );
